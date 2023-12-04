cd28f9e88b0c0a89bfff313cf91e7c175e335eca
Jason Florentino <jasonmflorentino@gmail.com>
2023-12-03 22:15:09 -0500

    new day script

---
+++ newDay.sh
@@ -0,0 +1,21 @@
+if [ -z "$1" ];
+  then 
+    echo "Error: Expected a day number, like 07."
+    exit 1;
+  else echo "Setting up new day $1..."; 
+fi
+
+
+echo "Making day directory..."
+mkdir $1
+
+echo "Creating files..."
+cd $1
+touch input
+echo "// https://adventofcode.com/2023/day/$1" > index.ts
+echo "// (cd $1; bun index.ts)" >> index.ts
+echo "" >> index.ts
+echo "import * as Util from \"../util\";" >> index.ts
+
+cd ..
+echo "Done!"
